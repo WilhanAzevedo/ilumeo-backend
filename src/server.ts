ef41c4cbd9986585f86a7f06f6da9f8edd1813d5
Wilhan Azevedo Dionizio <wilhan.azevedo@gmail.com>
2023-04-03 01:15:44 -0400

    Ajustes Server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import { app } from './app'
 
-app.listen(3333).on('listening', () => {
-  console.log('Server is running on port 3333')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .on('listening', () => {
+    console.log('Server is running')
+  })
